319c0e54a75c04d274d1b3ddc67b94ba02e73bd8
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-05-11 16:22:34 +0530

    Add Props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,97 +1,86 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price:999,
-            title: 'Phone',
-            qty: 1,
-            img: '',
-            
-        }
-        this.testing()
-       
+class CartItem extends React.Component {
+  increaseQuantity = () => {
+    // this.state.qty +=1;
+    // console.log(this.state.qty);
+    // setState one way
+    // this.setState({
+    //     qty: this.state.qty +1
+    // })
 
+    // setState second way (If previous state required so use this way)
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+//   testing() {
+//     const promise = new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve("done");
+//       }, 5000);
+//     });
+
+//     promise.then(() => {
+//       this.setState({ qty: this.state.qty + 10 });
+//       this.setState({ qty: this.state.qty + 10 });
+//       this.setState({ qty: this.state.qty + 10 });
+//       console.log("state", this.state);
+//     });
+//   }
+
+  decreaseQunatity = () => {
+    const { qty } = this.state;
+    if (qty === 0) {
+      return;
     }
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1,
+      };
+    });
+  };
 
-    increaseQuantity = () => {
-        // this.state.qty +=1;
-        // console.log(this.state.qty);
-        // setState one way
-        // this.setState({
-        //     qty: this.state.qty +1
-        // })
-
-        // setState second way (If previous state required so use this way)
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        })
-    }
-    testing (){
-        const promise = new Promise((resolve, reject) => {
-            setTimeout(()=> {
-                resolve('done');
-            } , 5000);
-        })
-
-        promise.then(() => {
-            this.setState({qty: this.state.qty + 10});
-            this.setState({qty: this.state.qty + 10});
-            this.setState({qty: this.state.qty + 10});
-            console.log('state' , this.state);
-
-        })
-
-
-    }
-
-    decreaseQunatity = ()=> {
-        const {qty} = this.state;
-        if(qty === 0 ){
-            return;
-        }
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty-1
-            }
-         } )
-    }
-
-    render (){
-        const { price, title, qty} = this.state;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-                </div>
-                <div className="right-block">
-                    <div style={ { fontSize: 25} }>{title}</div>
-                    <div style={ { color:'#777'} }>Rs {price}</div>
-                    <div style={ { color:'#777'} }>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons*/}
-                        <FontAwesomeIcon onClick={this.increaseQuantity} icon={icon({name: 'circle-plus'})} />
-                        <FontAwesomeIcon onClick={this.decreaseQunatity}  icon={icon({name: 'circle-minus'})} />
-                        <FontAwesomeIcon icon={icon({name: 'trash'})} />
-                       
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    console.log("this.props", this.props);
+    const { price, title, qty } = this.props.product;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons*/}
+            <FontAwesomeIcon
+              onClick={this.increaseQuantity}
+              icon={icon({ name: "circle-plus" })}
+            />
+            <FontAwesomeIcon
+              onClick={this.decreaseQunatity}
+              icon={icon({ name: "circle-minus" })}
+            />
+            <FontAwesomeIcon icon={icon({ name: "trash" })} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 export default CartItem;
--- src/cart.js
+++ src/cart.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-import CartItem from './CartItem';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import CartItem from "./CartItem";
 
-class Cart extends React.Component{
-    render(){
-        return (
-            <div className='cart'>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
-            </div>
-        )
-    }
+class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+            price: 999,
+            title: 'Watch',
+            qty: 1,
+            img: '',
+            id: 1
+        },
+        {
+            price: 700,
+            title: 'Mobile Phone',
+            qty: 10,
+            img: '',
+            id: 2
+        },
+        {
+            price: 378,
+            title: 'Laptop',
+            qty: 4,
+            img: '',
+            id: 3
+        }      
+
+      ]
+    };
+    
+  }
+  render() {
+    const {products} = this.state;
+    return (
+      <div className="cart">
+        {products.map((product) => {
+            return (
+                <CartItem 
+                    product = {product} 
+                    key={product.id} 
+                />
+            )
+        })}
+       
+      </div>
+    );
+  }
 }
-
 
 export default Cart;
